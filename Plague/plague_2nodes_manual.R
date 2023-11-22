4ca347d115397859d4d572b59a83918e01c9362b
Paola Lizarralde <dlizarra@eafit.edu.co>
2023-11-22 15:10:48 +0100

    Update plague_2nodes_manual.R
    Fix the event definition.

--- Plague/plague_2nodes_manual.R
+++ Plague/plague_2nodes_manual.R
@@ -16,50 +16,59 @@ compartments <- c("S", "E", "I", "R", "D")
 parameters <- c("beta", "mu")
 
 colnames(u0)<-compartments
+print(u0)
 
-#EMatrix
-E <- matrix(c(0, 0, 1, 0, 0), nrow = 5, ncol = 1, dimnames = list(c("S", "E", "I", "R", "D"), c("1")))
+tspan <- seq(from = 1, to = 400, by = 30)
 
+#EMatrix (#compartments x #events). For each column vector j we put 1 if the compartment
+# participated in the event j, otherwise zero.
+E <- matrix(c(0, 1, 1, 0, 0), nrow = 5, ncol = 1, dimnames = list(c("S", "E", "I", "R", "D"), c("event1")))
 
-# Create one enter event to introduce an infected individuals to the 2nd node at t = 50
-infect <- data.frame(event = "enter", time = 50, node = 1, dest = 2,
-                       n = 0, proportion = 0.25, select = 3, shift = 0)
-exposed <- data.frame(event = "enter", time = 10, node = 1, dest = 2,
-                      n = 0, proportion = 0.1, select = 2, shift = 0)
-events <- rbind(infect, exposed)
+print(E)
+# Create an external transfer event to move exposed or infected individuals 
+# from node 0 to node 1 at t = 60
+
+infect <- data.frame(event = "extTrans", time = 60, node = 1, dest = 2,
+                        n = 0, proportion = 0.25, select = 1, shift = 0)
+events <- rbind(infect)
 
 local_parameters <- data.frame(beta = c(0.7, 0.8),
                               mu = c(0.8, 0.4))   
 
 print(local_parameters)
 
-# n represents the number of nodes, on this case each node corresponds to one parish
-n <- 2
 
 model <- mparse(transitions = transitions,
                 compartments = compartments,
                 gdata = c(sigma = 0.17 , gamma = 0.4),
                 ldata = local_parameters,
                 u0 = u0,
-                events <- events,
                 E = E,
-                tspan = 1:250)
-
-
+                events = events,
+                tspan =tspan)
 set.seed(123)
 set_num_threads(1)
 result <- run(model = model)
+result
 
+# Cumulative deaths
 traj_D <- trajectory(model=result, compartments = "D")
 # Show the points per node
 View(traj_D)
 # Plot a specific trajectory
 ggplot(traj_D) + geom_line(aes(x=time,y=D,color=factor(node)))
 
-
+# Infected
 traj_I <- trajectory(model=result, compartments = "I")
 # Show the points per node
 View(traj_I)
 # Plot a specific trajectory
 ggplot(traj_I) + geom_line(aes(x=time,y=I,color=factor(node)))
+
+# Exposed
+traj_E <- trajectory(model=result, compartments = "E")
+# Show the points per node
+View(traj_E)
+# Plot a specific trajectory
+ggplot(traj_E) + geom_line(aes(x=time,y=E,color=factor(node)))
 
